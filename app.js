0de18304d9d9f679498438359ab5081c63620d90
Matei Adriel <rafaeladriel11@gmail.com>
2019-02-14 00:02:59 +0200

    fixed process.env checking

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const {QuickServer} = require("translucid");
 
-const {translucid} = QuickServer(8000 || process.env.PORT);
+const {translucid} = QuickServer(process.env.PORT || 8000);
 translucid.public("pool_pi3");
 translucid.bind("/","pool_pi3/index.html",["*"]);
